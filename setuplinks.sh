822d7f6b9f6513c98b59f8e9b99918fe3c183a96
June Tate-Gans <jtgans@furian.theonelab.com>
2008-09-25 10:42:45 -0700

    Making setuplinks remove .bash_logout as well.

--- setuplinks.sh
+++ setuplinks.sh
@@ -3,6 +3,7 @@
 [ -f $HOME/.bash_profile ] && rm $HOME/.bash_profile
 [ -f $HOME/.profile ] && rm $HOME/.profile
 [ -f $HOME/.bashrc ] && rm $HOME/.bashrc
+[ -f $HOME/.bash_logout ] && rm $HOME/.bash_logout
 
 ln -s bash_profile ../.bash_profile
 ln -s bashrc ../.bashrc
