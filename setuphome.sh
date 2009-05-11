505ab6354f7ad038ba511f56922fb4ca16f59192
June Tate-Gans <june@theonelab.com>
2009-05-11 13:25:14 -0700

    making setuphome.sh a bit more sane.

--- setuphome.sh
+++ setuphome.sh
@@ -1,13 +1,69 @@
 #!/bin/bash
 
-[ -z $(which git) ] && echo "git not available." && exit 1
+function die()
+{
+    echo "$@"
 
-ln -s /media/private Private
-mkdir Documents Downloads Code Projects SDKs chroots www
+    while popd 2>/dev/null >/dev/null; do
+        echo "$COMMAND_NAME: Leaving directory $(pwd)"
+    done
 
-git clone git://git.theonelab.com/dotemacs.git .emacs.d
-git clone git://git.theonelab.com/dotbash.git .bash.d
-git clone git://git.theonelab.com/dotfiles.git .dotfiles
+    exit 1
+}
 
-source .bash.d/setuplinks.sh
-source .dotfiles/setuplinks.sh
+function try()
+{
+    echo "$@"
+    "$@"
+
+    if [ "$?" != "0" ]; then
+        die "$COMMAND_NAME: Command failed: exit code $?"
+    fi
+}
+
+function in-dir()
+{
+    local dir=$1
+    shift
+
+    echo "$COMMAND_NAME: Entering directory $dir"
+    pushd $dir 2>/dev/null >/dev/null || die "$COMMAND_NAME: $dir does not exist."
+    "$@"
+    popd 2>/dev/null >/dev/null
+    echo "$COMMAND_NAME: Leaving directory $dir"
+}
+
+function require-command()
+{
+    local command=$1
+    shift
+
+    if [ -z $(which $command) ]; then
+        die "$@"
+    fi
+}
+
+function setup-dirs()
+{
+    try ln -s /media/private Private
+    try mkdir Documents Downloads Code Projects SDKs chroots www
+}
+
+function setup-repos()
+{
+    try git clone git://git.theonelab.com/dotemacs.git .emacs.d
+    try git clone git://git.theonelab.com/dotbash.git .bash.d
+    try git clone git://git.theonelab.com/dotfiles.git .dotfiles
+}
+
+function setup-links()
+{
+    try source .bash.d/setuplinks.sh
+    try source .dotfiles/setuplinks.sh
+}
+
+require git "git not available."
+
+in-dir $HOME setup-dirs
+in-dir $HOME setup-repos
+in-dir $HOME setup-links
