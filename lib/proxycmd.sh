b4b84fd3447a25a5d2d33e23286a9cb214b3a873
June R. Tate-Gans <june@theonelab.com>
2010-07-15 16:42:02 -0600

    Adding in proxycmd.

---
+++ lib/proxycmd.sh
@@ -0,0 +1,17 @@
+proxycmd() {
+    local proxy_user;
+    local proxy_pass;
+    
+    read -p "User: " proxy_user;
+    read -sp "Password: " proxy_pass; 
+    echo;
+    
+    export http_proxy="http://$proxy_user:$proxy_pass@corp-hts-proxy.mhc.:8080";
+    export RSYNC_PROXY="http://$proxy_user:$proxy_pass@corp-hts-proxy.mhc.:8080";
+    
+    $@;
+    
+    export http_proxy="";
+    export RSYNC_PROXY="";
+}
+
