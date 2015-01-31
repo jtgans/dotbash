07054ed06e7b6a3ff7dc4e9daa8095d3f516a021
June Tate-Gans <june@theonelab.com>
2015-01-30 16:53:01 -0800

    Fixing lots of junk in darwin.

--- sys/darwin.sh
+++ sys/darwin.sh
@@ -20,20 +20,14 @@ alias ls='ls -Gsph'
 alias ll='ls -l'
 alias la='ls -A'
 
-export PROMPT_COMMAND='history -a'
-export PATH="${HOME}/.bin:${PATH}:/opt/local/bin"
+export PROMPT_COMMAND='history -a; history -n'
+export PATH="${HOME}/.bin:${PATH}"
 export PAGER="/usr/bin/less"
-export EDITOR=""
-export PS1='\h:\w\$ '
 
-if [ "$TERM" == "dtterm" ]; then
-  export TERM="xterm"       # Makes terminfo happy
-
-  alias dock='echo -ne "\E[2t"'
-  alias raise='echo -ne "\E[5t"'
-  alias 132x30='echo -ne "\E[8;132;30t"'
-  alias 80x30='echo -ne "\E[8;80;30t"'
-  alias maxh='echo -ne "\E[8;0;80t"'
-  alias maxw='echo -ne "\E[8;24;0t"'
-  alias bgmake='dock; make \~*; raise'
+if [[ ! -z $(which emacsclient) ]]; then
+    export EDITOR="$(which emacsclient) -c -a $(which vim || which vi)"
+else
+    export EDITOR="$(which vim || which vi)"
 fi
+
+export PS1='\h:\w\$ '
